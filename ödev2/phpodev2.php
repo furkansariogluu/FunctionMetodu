4f13b85b66c944d690543b488e7e54e316275ef6
Furkan SARIOĞLU <82783845+furkansariogluu@users.noreply.github.com>
2021-11-18 03:00:39 +0300

    FunctionMetodu Yapıldı

---
+++ ödev2/phpodev2.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function bilgiler ($bilgi){
+    echo $bilgi;
+    return $bilgi;
+}
+
+bilgiler("<h3> Ad: Furkan </h3>");
+bilgiler("<h3> Soyad: SARIOĞLU </h3>");
+bilgiler("<h3> Okul: Adnan Menderes Üniversitesi </h3>");
+bilgiler("<h3> Program: Bilgisayar Programcılığı </h3>");
+bilgiler("<h3> Mail: 206001056@stu.adu.edu.tr </h3> ");
+bilgiler("<h3> Ders: İnternet Programcılığı </h3> ");
+
+
+
+
+
+?>
